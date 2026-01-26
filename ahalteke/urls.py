759128eece5d7f26b36eb7f853b33d7af6f07c55
sahinbingol85 <sahinbingol@gmail.com>
2026-01-26 22:37:41 +0300

    Add files via upload

--- ahalteke/urls.py
+++ ahalteke/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+# turnuvalar view'ını da import etmeyi unutma:
+from core.views import index, turnuvalar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'), # Ana sayfa
+    # Yeni sayfa için yol:
+    path('turnuvalar/', turnuvalar, name='turnuvalar'),
 ]
