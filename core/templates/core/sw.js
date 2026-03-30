513fea130c4567042b5248be5c2b198f6cb5e69b
sahinbingol85 <sahinbingol@gmail.com>
2026-03-30 13:47:22 +0300

    Update service worker installation and fetch events

--- core/templates/core/sw.js
+++ core/templates/core/sw.js
@@ -1,9 +1,17 @@
 self.addEventListener('install', (event) => {
-    console.log('Ahal Teke Hakem Uygulaması Kuruldu!');
+    console.log('Ahal Teke Rezervasyon PWA Motoru Kuruldu!');
     self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
-    // Sitenin normal şekilde çalışmasına izin verir
+    // Sitenin normal şekilde canlı çalışmasına izin verir (Önbellekten eski veri göstermez)
+    event.respondWith(fetch(event.request));
+});self.addEventListener('install', (event) => {
+    console.log('Ahal Teke Rezervasyon PWA Motoru Kuruldu!');
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    // Sitenin normal şekilde canlı çalışmasına izin verir (Önbellekten eski veri göstermez)
     event.respondWith(fetch(event.request));
 });
