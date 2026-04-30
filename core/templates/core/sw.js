3f065bfac1e658a89d268779f0bb96079f1bdce0
sahinbingol85 <sahinbingol@gmail.com>
2026-04-30 13:53:00 +0300

    Add files via upload

--- core/templates/core/sw.js
+++ core/templates/core/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ahalteke-v2';
+const CACHE_NAME = 'ahalteke-v3'; // v3 yaptık ki cihazlar eski bozuk versiyonu hemen unutup bunu kursun
 
 // Kurulum Aşaması
 self.addEventListener('install', (e) => {
@@ -12,8 +12,16 @@ self.addEventListener('activate', (e) => {
     return self.clients.claim();
 });
 
-// İnternet kesilirse diye basit bir dinleyici (Uygulamanın yüklenmesini tetikler)
+// İnternet isteklerini dinleme
 self.addEventListener('fetch', (e) => {
+    
+    // EKLENEN HAYAT KURTARICI KOD: 
+    // Sadece GET (sayfa yükleme) isteklerine karış. 
+    // POST (Form gönderme, Giriş yapma) isteklerini direkt Django'ya bırak!
+    if (e.request.method !== 'GET') {
+        return; 
+    }
+
     e.respondWith(
         fetch(e.request).catch(() => {
             return caches.match(e.request);
